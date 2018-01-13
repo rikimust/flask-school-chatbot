2d5b9865405c35ee64db93b4fb457a4324345688
iLizardKing <itlizardking@gmail.com>
2018-01-13 22:26:01 +0000

    Add request log with .csv-file

--- flask_app.py
+++ flask_app.py
@@ -1,41 +1,54 @@
+import csv, os
 from flask import Flask, request, json
+from datetime import datetime, timezone, timedelta
+
 from settings import token, confirmation_token
+from vkapi import api
 import messageHandler
 import view
+
 
 app = Flask(__name__)
 
 
-def log_request(msg):
-    line = ''
-    f = open('requests.log', 'a', encoding='utf-8')
-    for key, value in msg.items():
-        line += '{}: {},'.format(key, value)
-        print(line, file=f)
-    f.close()
+def log_request(msg: 'vk_message_obj', filename: str='chat-bot/requests.log') -> None:
+    headers = ['date','time','name','body']
+    with open(filename, 'a') as log:
+        writer = csv.DictWriter(log, delimiter=';', fieldnames=headers)
+        if os.stat(filename).st_size == 0:
+            writer.writeheader()
+        # datetime_now = datetime.now(tz=timezone(timedelta(hours=3)))
+        dt = datetime.fromtimestamp(msg['date'], tz=timezone(timedelta(hours=3)))
+        user = api.users.get(user_id=msg['user_id'])[0]
+        formed_line = {
+            'date': '{:%d.%m.%Y}'.format(dt),
+            'time': '{:%H:%M:%S}'.format(dt),
+            'name': '{} {}'.format(user['last_name'].title(), user['first_name'].title()),
+            'body': msg['body'] }
+        writer.writerow(formed_line)
 
 @app.route('/index')
 @app.route('/')
-def index_page():
+def index_page() -> 'html':
     return view.index_page()
 
 @app.route('/info')
-def info_page():
+def info_page() -> 'html':
     return view.info_page()
 
 @app.route('/log')
-def log_page():
+def log_page() -> 'html':
     return view.log_page()
 
 @app.route('/', methods=['POST'])
-def processing():
+def processing() -> str:
     data = json.loads(request.data)
     if 'type' not in data.keys():
         return 'not vk'
     if data['type'] == 'confirmation':
         return confirmation_token
     elif data['type'] == 'message_new':
-        log_request(data)
+        log_request(data['object'])
         messageHandler.create_answer(data['object'], token)
         return 'ok'
 
--- vkapi.py
+++ vkapi.py
@@ -1,7 +1,7 @@
 import vk
 
 session = vk.Session()
-api = vk.API(session, v=5.0)
+api = vk.API(session, v=5.0, lang='ru')
 
 def send_message(user_id, token, message, attachment=""):
     api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
