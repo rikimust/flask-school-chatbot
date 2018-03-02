1dd7bb4870baf2b6e44b0c8cc3b64133ea567bf1
iLizardKing <eakuzmin87@mail.ru>
2018-03-02 18:36:45 +0300

    Refactoring commands system. info-command - v2

--- commands/info.py
+++ commands/info.py
@@ -11,22 +11,21 @@ def help(keyword=''):
 help_command = command_system.Command()
 help_command.keys = keyword_gen('помощь')
 help_command.alias = keyword_gen('помоги', 'help', 'хелп', 'pomosh')
-help_command.description = 'Покажу список команд'
+help_command.description = 'Покажу список команд и их описание'
 help_command.process = help
 
 def all_commands(keyword=''):
     message = ''
     for command in command_system.command_list:
+        # все чётные комманды - это транслит на нечётные
         message += '{}\n'.format(command.keys[0].upper())
-        if command.keys[1:] is not []:
-            for key in command.keys[1:]:
-                if key[0] not in "qwertyuiop[]asdfghjkl;\'zxcvbnm,./":
-                    message += '{}, '.format(key)
-            message = message[:-1] + '.\n'
+        for i in range(0, len(command.aliases), 2):
+            message += '{}, '.format(command.aliases[i])
+        message = message[:-1] + '.\n'
     return message, ''
 
 all_commands_command = command_system.Command()
 all_commands_command.keys = keyword_gen('команды')
 all_commands_command.alisas = keyword_gen('все команды', 'список команд')
-all_commands_command.description = 'Покажу список всех команд'
+all_commands_command.description = 'Покажу все команды и их псевдонимы'
 all_commands_command.process = all_commands
